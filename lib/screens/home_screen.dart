8eca5f0fc7494463420c732f5d46e48b30b72c2c
kathya marquez <knmarquez25@gmail.com>
2020-10-22 19:29:01 -0700

    commited changes

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:love_calculator_3/widgets/names_container.dart';
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'results_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  final TextEditingController name1 = TextEditingController();
+  final TextEditingController name2 = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.red,
+      appBar: AppBar(
+        title: Text(
+          'Love Calculator',
+        ),
+        elevation: 0.0,
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: NamesContainer(
+          name1: name1,
+          name2: name2,
+          onTap: () async {
+            var url =
+                'https://rapidapi.p.rapidapi.com/getPercentage?fname=${name1.text.trim()}&sname=${name2.text.trim()}';
+            Map<String, String> headers = {
+              'x-rapidapi-host': 'love-calculator.p.rapidapi.com',
+              'x-rapidapi-key':
+              '94b2f46f82msh210a8553e8177dbp1b1f0ejsneb11a4ec0841'
+            };
+
+            var response = await http.get(url, headers: headers);
+            var jsonResponse = jsonDecode(response.body);
+
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => ResultsScreen(
+                      percent: int.parse(jsonResponse['percentage']),
+                      message: jsonResponse['result'],
+                    )));
+          },
+        ),
+      ),
+    );
+  }
+}
