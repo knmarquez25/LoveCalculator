8f72711d7a41e43b83cbf41e22c6c4e7ca03688b
kathya marquez <knmarquez25@gmail.com>
2020-10-22 19:49:32 -0700

    finalize app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:lovecalculator/main.dart';
+import 'package:love_calculator_3/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
